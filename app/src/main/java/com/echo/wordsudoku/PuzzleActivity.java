fb95ac074f66c0147c23925bf0de4b36144e6c6e
eakbarib <eakbarib@sfu.ca>
2023-02-05 15:52:57 -0800

    Both Activities (Main and Puzzle) have a good-looking layout. Further, clicking "New Game" and  "Load Game", takes the user to  the Puzzle Activity and using  intent extras it realizes whether  it should start a new game or load  the previous game.

--- app/src/main/java/com/echo/wordsudoku/PuzzleActivity.java
+++ app/src/main/java/com/echo/wordsudoku/PuzzleActivity.java
@@ -2,28 +2,53 @@ package com.echo.wordsudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PuzzleActivity extends AppCompatActivity {
+
+    private static final String LOAD_PREVIOUS_GAME = "com.echo.wordsudoku.load_previous_game";
+
+    // Action text view shows whether a new game is being started or a previous game is being loaded
+    // This is just for testing purposes
+    private TextView mActionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
-        Toast.makeText(this, "Puzzle Activity being Created (Check the memory and if there was a game in progress load it!)", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Puzzle Activity being Created (Load previous game!)", Toast.LENGTH_LONG).show();
+
+        // get the ui reference
+        mActionTextView = findViewById(R.id.action_text_view);
+
+        // set the text based on the boolean passed in as an extra in the intent
+        if (getIntent().getBooleanExtra(LOAD_PREVIOUS_GAME, false)) {
+            mActionTextView.setText("Loading previous game...");
+        } else {
+            mActionTextView.setText("Starting new game...");
+        }
     }
 
 
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "Puzzle Activity Started (Check the memory and if there was a game in progress load it!)", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Puzzle Activity Started (Load previous game!)", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this, "Puzzle Activity Stopped (Save game progress now!)", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Puzzle Activity Stopped (Save game progress now!)", Toast.LENGTH_LONG).show();
+    }
+
+    public static Intent newIntent (Context packageContext, boolean loadPreviousGame) {
+        Intent intent = new Intent(packageContext, PuzzleActivity.class);
+        intent.putExtra(LOAD_PREVIOUS_GAME, loadPreviousGame);
+        return intent;
     }
 }
