7bab3e32aae3580ed71b6726c96b5260023c2c3c
KoushaAmouzesh <kousha82@@students.cs.ubc.ca>
2023-02-05 16:10:01 -0800

    test commit on my local branch add Board model

---
+++ app/src/main/java/com/echo/wordsudoku/models/Board.java
@@ -0,0 +1,12 @@
+package com.echo.wordsudoku.models;
+
+import java.util.ArrayList;
+
+public class Board {
+
+    ArrayList<ArrayList<String>> board;
+
+    public Board() {
+        board = new ArrayList<>();
+    }
+}
