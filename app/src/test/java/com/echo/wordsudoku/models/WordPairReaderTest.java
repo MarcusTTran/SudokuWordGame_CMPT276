957cc8f8c3d1385eacdc539322ebd526d52bb5c3
Skye Davidson <sda166@sfu.ca>
2023-02-21 22:14:21 -0800

    Add WordPairReaderTest class

---
+++ app/src/test/java/com/echo/wordsudoku/models/WordPairReaderTest.java
@@ -0,0 +1,26 @@
+package com.echo.wordsudoku.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileReader;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+class WordPairReaderTest {
+
+    @Test
+    void testConstructor() {
+
+    }
+
+    @Test
+    void getWords() {
+    }
+}
