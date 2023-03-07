4cabdccb9b39e782dae27fe10610e21ca445d8b4
eakbarib <eakbarib@sfu.ca>
2023-03-06 16:04:05 -0800

    Add todos comments

--- app/src/main/java/com/echo/wordsudoku/ui/destinations/ChooseCustomWordsFragment.java
+++ app/src/main/java/com/echo/wordsudoku/ui/destinations/ChooseCustomWordsFragment.java
@@ -1,11 +1,30 @@
 package com.echo.wordsudoku.ui.destinations;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.echo.wordsudoku.R;
+import com.echo.wordsudoku.ui.puzzleParts.PuzzleViewModel;
 
 public class ChooseCustomWordsFragment extends Fragment{
-    public ChooseCustomWordsFragment() {
-        super(R.layout.fragment_choose_custom_words);
+
+    private PuzzleViewModel mPuzzleViewModel;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_choose_custom_words, container, false);
+
+        mPuzzleViewModel = new ViewModelProvider(requireActivity()).get(PuzzleViewModel.class);
+
+        // TODO: Get the values of the word pairs user entered and set them in the PuzzleViewModel
+        // EditText editText = root.findViewById(R.id.editText);
+        // mPuzzleViewModel.setWordPairs(editText.getText().toString());
+
+        return root;
     }
 }
